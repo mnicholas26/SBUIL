d845b1918330403c9babc700f389c427a7217a03
mnicholas26 <spiritbeast26@yahoo.co.uk>
2021-05-12 12:09:16 +0100

    colour class start

--- components.js
+++ components.js
@@ -983,6 +983,477 @@ window.onload = () => {
 
     }
 
+    // function createColourPicker(elem, options){
+    //     //check if options exists
+    //     if(options == undefined) options = {}; 
+
+    //     //create dom
+    //     let trolley = document.createElement('div');
+    //     trolley.classList.add('trolley');
+    //     let range = document.createElement('div');
+    //     range.classList.add('range');
+    //     let nodule = document.createElement('div');
+    //     nodule.classList.add('nodule');
+    //     let label = document.createElement('div');
+    //     label.classList.add('label');
+
+    //     trolley.appendChild(range);
+    //     nodule.appendChild(label);
+    //     trolley.appendChild(nodule);
+    //     elem.appendChild(trolley);
+
+    //     //define properties with getters and setters
+    //     Object.defineProperty(elem, 'round', {
+    //         get: () => {
+    //             return elem._round;
+    //         },
+    //         set: (val) => {
+    //             if(typeof val != 'number') console.error('Slider Round value must be a number');
+    //             else elem._round = val;
+    //         }
+    //     });
+
+    //     Object.defineProperty(elem, 'min', {
+    //         get: () => {
+    //             return elem._min;
+    //         },
+    //         set: (val) => {
+    //             if(typeof val != 'number') console.error('Slider Min value must be a number');
+    //             else elem._min = val;
+    //         }
+    //     });
+
+    //     Object.defineProperty(elem, 'max', {
+    //         get: () => {
+    //             return elem._max;
+    //         },
+    //         set: (val) => {
+    //             if(typeof val != 'number') console.error('Slider Max value must be a number');
+    //             else elem._max = val;
+    //         }
+    //     });
+
+    //     Object.defineProperty(elem, 'notches', {
+    //         get: () => {
+    //             return elem._notches;
+    //         },
+    //         set: (val) => {
+    //             if(typeof val != 'number') console.error('Slider Notches value must be a number');
+    //             else elem._notches = val;
+    //         }
+    //     });
+
+    //     Object.defineProperty(elem, 'direction', {
+    //         get: () => {
+    //             return elem._direction;
+    //         },
+    //         set: (val) => {
+    //             if(typeof val != 'string') console.error('Slider Direction value must be a string');
+    //             else{
+    //                 let [dir, rev] = val.split('-');
+    //                 if(!(dir == 'row' || dir == 'column') || (rev != undefined && rev != 'reverse')){
+    //                     console.error('Slider Direction must be either: row, column, row-reverse or column-reverse');
+    //                 } else {
+    //                     elem._direction = dir;
+    //                     elem.classList.remove('row', 'column', 'reverse');
+    //                     elem.classList.add(dir);
+    //                     if(rev) elem.classList.add(rev);
+    //                     elem.reversed = (rev) ? true : false;
+    //                 }
+    //             }
+    //         }
+    //     });
+
+    //     //define methods
+        
+
+    //     Object.defineProperty(elem, 'value', {
+    //         get: () => {
+    //             return elem._value;
+    //         },
+    //         set: (val) => {
+    //             if(typeof val != 'number') console.error('Slider Value must be a number');
+    //             else {
+    //                 let value = Math.max(Math.min(val, 1), 0);
+    //                 value = elem.notch(value);
+    //                 if(elem._value != value || elem.environment.final){
+    //                     elem._value = value;
+    //                     elem.displayValue = (elem.value * (elem.max - elem.min)).toFixed(elem.round);
+    //                     elem.render();
+    //                     elem.fireEvent('change', {value: value, dvalue: elem.displayValue});
+    //                 }
+    //             }
+    //         }
+    //     });
+
+    //     //set properties
+    //     elem.round = (options.round) ? options.round : 0;
+    //     elem.min = (options.min) ? options.min : 0;
+    //     elem.max = (options.max) ? options.max : 100;
+    //     elem.notches = (options.notches) ? options.notches : 0;
+    //     elem.direction = (options.direction) ? options.direction : 'row';
+    //     elem.value = (options.value) ? options.value : 0.5;
+
+    //     //set local environment
+    //     elem.environment = {
+    //         ...elem.environment,
+    //         final: false,
+    //     }
+
+    //     //define user events and functionality
+    //     elem.addEventListener('mousedown', (e) => {
+    //         e.preventDefault();
+    //         elem.trolleyBBox = trolley.getBoundingClientRect();
+    //         let valuefn;
+    //         if(elem.direction == 'row'){
+    //             if(elem.reversed){
+    //                 valuefn = (e) => (elem.trolleyBBox.x + elem.trolleyBBox.width - e.clientX)/elem.trolleyBBox.width;
+    //             } else {
+    //                 valuefn = (e) => (e.clientX - elem.trolleyBBox.x)/elem.trolleyBBox.width;
+    //             }
+    //         } else {
+    //             if(elem.reversed){
+    //                 valuefn = (e) => (e.clientY - elem.trolleyBBox.y)/elem.trolleyBBox.height;
+    //             } else {
+    //                 valuefn = (e) => (elem.trolleyBBox.y + elem.trolleyBBox.height - e.clientY)/elem.trolleyBBox.height;
+    //             }
+    //         }
+
+    //         elem.environment.final = false;
+    //         elem.environment.user = true;
+    //         elem.value = valuefn(e);
+    //         elem.addEventListener('mousemove', drag);
+    //         window.addEventListener('mouseup', enddrag);
+    //         window.addEventListener('mouseleave', enddrag);
+
+    //         function drag(e){
+    //             elem.value = valuefn(e);
+    //         }
+
+    //         function enddrag(e){
+    //             elem.environment.final = true;
+    //             elem.value = valuefn(e);
+    //             elem.environment.user = false;
+    //             elem.removeEventListener('mousemove', drag);
+    //             window.removeEventListener('mouseup', enddrag);
+    //             window.removeEventListener('mouseleave', enddrag);
+
+    //         }
+    //     });
+
+    // }
+
+    //USEFUL FUNCTIONS
+
+    //COLOUR STUFF
+    function hexToRGBA(str, alpha = false, opacity){
+        let hexreg = /#[abcdef\d]{8}|#[abcdef\d]{6}/i;
+        let reg = /\w{2}/g || [];
+        if(hexreg.test(str)){
+            let [r,g,b,a] = str.match(reg);
+            if(a == undefined) a = 'ff';
+            r = parseInt(r, 16);
+            g = parseInt(g, 16);
+            b = parseInt(b, 16);
+            a = opacity != undefined ? opacity : parseInt(a, 16)/255;
+            let val = 'rgb';
+            if(alpha) val += 'a'
+            val += `(${r}, ${g}, ${b}`
+            if(alpha) val += `, ${a.toFixed(2)}`;
+            val += ')';
+            return {
+                r: r,
+                g: g,
+                b: b,
+                a: a,
+                val: val
+            }
+        } else {
+            console.error(`String: ${str} is not a correctly formatted hex colour string`);
+            return false;
+        }
+    }
+
+    class SBColour{
+        _r = 0;
+        _g = 0;
+        _b = 0;
+        _a = 1;
+        _h = 0;
+        _s = 0;
+        _l = 0;
+        _type = 'rgb';
+        _alpha = false;
+        _val = undefined;
+        _hex = '#000000';
+        _css = 'black';
+
+        constructor(type = 'rgb', r = 0, g = 0, b = 0, a = 1){
+            if(type == 'rgb'){
+                //
+            } else if(type = 'hsl'){
+                //
+            } else {
+                console.error(`Colour type must be 'rgb' or 'hsl'. Value provided: ${type}`);
+            }
+            _r = r;
+            _g = g;
+            _b = b;
+            _type = type;
+            _a = a;
+        }
+
+        static RGBtoHSL(...args){
+            let r, g, b, a, alpha = false, opts = {};
+
+            if(args.length > 1 && typeof args[args.length - 1] == 'object' && args[args.length - 1] != null) opts = args.pop();
+            let options = {
+                round: (opts.round) ? true : false,
+                decimal: (opts.decimal) ? true : false
+            }
+
+            if(args.length == 1 && typeof args[0] == 'object' && args[0] != null){
+                if(args[0].r != undefined && args[0].g != undefined && args[0].b != undefined){
+                    r = args[0].r;
+                    g = args[0].g;
+                    b = args[0].b;
+                    if(args[0].a != undefined){
+                        alpha = true;
+                        a = args[0].a;
+                    }
+                } else console.error(`Passed object requires 'r', 'g' and 'b' properties`);
+            } else if(args.length == 3) {
+                [r, g, b] = args;
+            } else if(args.length == 4) {
+                alpha = true;
+                [r, g, b, a] = args;
+            } else {
+                console.error(`Arguement error: Please pass either an object with RGB properties or r, g, b and possibly a values`);
+                return;
+            }
+
+            //sanity checking
+            if(typeof r == 'number'){
+                if(r < 0 || r > 255){
+                    console.error(`'r' value must be between 0 and 255, provided value: ${r}`);
+                    return;
+                }
+            } else console.error(`'r' value must be a number, provided type: ${typeof r}`);
+            if(typeof g == 'number'){
+                if(g < 0 || g > 255){
+                    console.error(`'g' value must be between 0 and 255, provided value: ${g}`);
+                    return;
+                }
+            } else console.error(`'g' value must be a number, provided type: ${typeof g}`);
+            if(typeof b == 'number'){
+                if(b < 0 || b > 255){
+                    console.error(`'b' value must be between 0 and 255, provided value: ${b}`);
+                    return;
+                }
+            } else console.error(`'b' value must be a number, provided type: ${typeof b}`);
+            if(a != undefined){
+                if(typeof a == 'number'){
+                    if(a < 0 || a > 1){
+                        console.error(`if 'a' value is provided it must be between 0 and 1, provided value: ${a}`);
+                        return;
+                    }
+                } else console.error(`if 'a' value is provided it must be a number, provided type: ${typeof a}`);
+            }
+
+            let rr = r/255;
+            let gg = g/255;
+            let bb = b/255;
+            let cmax = Math.max(rr, gg, bb);
+            let cmin = Math.min(rr, gg, bb);
+            let delta = cmax - cmin;
+
+            let h, s, l;
+            if(delta == 0) h = 0;
+            else if(cmax == rr) h = 60 * ((((gg - bb) / delta) + 6) % 6);
+            else if(cmax == gg) h = 60 * (((bb - rr) / delta) + 2);
+            else if(cmax == bb) h = 60 * (((rr - gg) / delta) + 4);
+
+            l = (cmax + cmin) / 2;
+
+            if(delta == 0) s = 0;
+            else s = delta / (1 - Math.abs((2 * l) - 1));
+
+            // let out = {
+            //     h: Math.round(h),
+            //     s: Math.round(s * 1000) / 10,
+            //     l: Math.round(l * 1000) / 10
+            // }
+
+            let out = {
+                h: options.round ? Math.round(h) : h,
+                s: options.decimal ? s : options.round ? Math.round(s * 1000) / 10 : s * 100,
+                l: options.decimal ? l : options.round ? Math.round(l * 1000) / 10 : l * 100
+            }
+
+            if(alpha) out.a = a;
+
+            return out;
+        }
+
+        static HSLtoRGB(...args){
+            let h, s, l, a, alpha = false, opts = {};
+
+            if(args.length > 1 && typeof args[args.length - 1] == 'object' && args[args.length - 1] != null) opts = args.pop();
+            let options = {
+                round: (opts.round) ? true : false,
+                decimal: (opts.decimal) ? true : false
+            }
+
+            if(args.length == 1 && typeof args[0] == 'object' && args[0] != null){
+                if(args[0].h != undefined && args[0].s != undefined && args[0].l != undefined){
+                    h = args[0].h;
+                    s = args[0].s;
+                    l = args[0].l;
+                    if(args[0].a != undefined){
+                        alpha = true;
+                        a = args[0].a;
+                    }
+                } else console.error(`Passed object requires 'h', 's' and 'l' properties`);
+            } else if(args.length == 3) {
+                [h, s, l] = args;
+            } else if(args.length == 4) {
+                alpha = true;
+                [h, s, l, a] = args;
+            } else {
+                console.error(`Arguement error: Please pass either an object with HSL properties or h, s, l and possibly a values`);
+                return;
+            }
+
+            //sanity checking
+            if(typeof h == 'number'){
+                if(h < 0 || h > 360){
+                    console.error(`'h' value must be between 0 and 360, provided value: ${h}`);
+                    return;
+                }
+            } else console.error(`'h' value must be a number, provided type: ${typeof h}`);
+            if(typeof s == 'number'){
+                if(s < 0 || s > 100){
+                    console.error(`'s' value must be between 0 and 100, provided value: ${s}`);
+                    return;
+                }
+            } else console.error(`'s' value must be a number, provided type: ${typeof s}`);
+            if(typeof l == 'number'){
+                if(l < 0 || l > 100){
+                    console.error(`'l' value must be between 0 and 100, provided value: ${l}`);
+                    return;
+                }
+            } else console.error(`'l' value must be a number, provided type: ${typeof l}`);
+            if(a != undefined){
+                if(typeof a == 'number'){
+                    if(a < 0 || a > 1){
+                        console.error(`if 'a' value is provided it must be between 0 and 1, provided value: ${a}`);
+                        return;
+                    }
+                } else console.error(`if 'a' value is provided it must be a number, provided type: ${typeof a}`);
+            }
+
+            let r, g, b;
+            if(!options.decimal){
+                s /= 100;
+                l /= 100;
+            }
+            
+            let c = (1 - Math.abs((2 * l) - 1)) * s;
+            let x = c * (1 - Math.abs(((h / 60) % 2) - 1));
+            let m = l - (c/2);
+
+            if(h < 60) [r, g, b] = [c, x, 0];
+            else if(h < 120) [r, g, b] = [x, c, 0];
+            else if(h < 180) [r, g, b] = [0, c, x];
+            else if(h < 240) [r, g, b] = [0, x, c];
+            else if(h < 300) [r, g, b] = [x, 0, c];
+            else (h < 360) [r, g, b] = [c, 0, x];
+
+            r = (r + m) * 255;
+            g = (g + m) * 255;
+            b = (b + m) * 255;
+
+            let out = {
+                r: options.round ? Math.round(r) : r,
+                g: options.round ? Math.round(g) : g,
+                b: options.round ? Math.round(b) : b
+            }
+
+            if(alpha) out.a = a;
+
+            return out;
+        }
+
+        static RGBtoHEX(...args){
+            let r, g, b, a, alpha = false, opts = {};
+
+            if(args.length > 1 && typeof args[args.length - 1] == 'object' && args[args.length - 1] != null) opts = args.pop();
+            let options = {}
+
+            if(args.length == 1 && typeof args[0] == 'object' && args[0] != null){
+                if(args[0].r != undefined && args[0].g != undefined && args[0].b != undefined){
+                    r = args[0].r;
+                    g = args[0].g;
+                    b = args[0].b;
+                    if(args[0].a != undefined){
+                        alpha = true;
+                        a = args[0].a;
+                    }
+                } else console.error(`Passed object requires 'r', 'g' and 'b' properties`);
+            } else if(args.length == 3) {
+                [r, g, b] = args;
+            } else if(args.length == 4) {
+                alpha = true;
+                [r, g, b, a] = args;
+            } else {
+                console.error(`Arguement error: Please pass either an object with RGB properties or r, g, b and possibly a values`);
+                return;
+            }
+
+            //sanity checking
+            if(typeof r == 'number'){
+                if(r < 0 || r > 255){
+                    console.error(`'r' value must be between 0 and 255, provided value: ${r}`);
+                    return;
+                }
+            } else console.error(`'r' value must be a number, provided type: ${typeof r}`);
+            if(typeof g == 'number'){
+                if(g < 0 || g > 255){
+                    console.error(`'g' value must be between 0 and 255, provided value: ${g}`);
+                    return;
+                }
+            } else console.error(`'g' value must be a number, provided type: ${typeof g}`);
+            if(typeof b == 'number'){
+                if(b < 0 || b > 255){
+                    console.error(`'b' value must be between 0 and 255, provided value: ${b}`);
+                    return;
+                }
+            } else console.error(`'b' value must be a number, provided type: ${typeof b}`);
+            if(a != undefined){
+                if(typeof a == 'number'){
+                    if(a < 0 || a > 1){
+                        console.error(`if 'a' value is provided it must be between 0 and 1, provided value: ${a}`);
+                        return;
+                    }
+                } else console.error(`if 'a' value is provided it must be a number, provided type: ${typeof a}`);
+            }
+
+            let hex = '#';
+            hex += r.toString(16) + g.toString(16) + b.toString(16);
+            if(alpha) {
+                a*=255
+                hex += a.toString(16);
+            }
+
+            return hex;
+        }
+
+    }
+
+    function stringToColour(str){
+        //
+    }
+
     function printEventObj(e){
         console.log(e)
     }
@@ -1007,4 +1478,15 @@ window.onload = () => {
     testdslider.setup();
     testdslider.addListener('change', printEventObj, {final: true, user: true});
     testdslider.value = {min: 0.1, max: 0.9};
+
+    let testhex = '#b22b64'
+    let testrgba = hexToRGBA(testhex);
+    console.log(testrgba);
+    document.body.style.backgroundColor = testrgba.val;
+
+    console.log(SBColour.RGBtoHSL(200, 171, 248));
+    console.log(SBColour.HSLtoRGB(62, 84, 53));
+    console.log(SBColour.HSLtoRGB(SBColour.RGBtoHSL(127, 16, 222)));
+
+    console.log(SBColour.RGBtoHEX(38, 77, 244));
 }
